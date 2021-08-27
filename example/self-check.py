e86542da5e6c5b3d283b118fe4211b8fc888791f
Decave <decave27@gmail.com>
2021-08-27 11:06:59 +0900

    fix: fixing example

--- example/self-check.py
+++ example/self-check.py
@@ -1,4 +1,4 @@
-# 자가진단을 처음 하는 유저 가입 및 자가진단 실행 예제
+# 자가진단 예제
 
 from hcspy import HCSClient
 from asyncio import run
@@ -6,7 +6,7 @@ from asyncio import run
 
 async def self_check():
     client = HCSClient()
-    school = await client.search_school(name="신일중학교", level="중학교", area="")
+    school = await client.search_school(name="학교 이름", level="학교 레벨/유형", area="지역")
     user = await client.login(
         school=school[-1],  # 검색한 학교들 중 최상위에 있는 학교 가져오기
         name="사용자 이름",
@@ -14,9 +14,6 @@ async def self_check():
         password="4자리 비밀번호",
     )
     user1 = user[-1]  # 첫번째 유저 가져오기
-    await user1.update_agreement()  # 약관동의
-    if not await user1.password_exist():  # 비밀번호가 없는 경우
-        await user1.register_password("비밀번호 4자리")  # 비밀번호 생성
     await user1.check(option1=False, option2=False, option3=False)  # 자가진단 실행
 
 
