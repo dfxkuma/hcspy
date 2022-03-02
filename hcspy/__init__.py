6090e08601f02f01a91f1f30fd97e2da5972cbb0
decave27 <decave27@gmail.com>
2022-03-02 17:16:25 +0900

    update v1.2.0

--- hcspy/__init__.py
+++ hcspy/__init__.py
@@ -4,5 +4,5 @@ from .model import Board, School
 from .user import User
 from .utils import *
 
-__version__ = "1.0.6"
+__version__ = "1.2.0"
 __all__ = ("HCSClient", "HTTPClient", "HTTPRequest", "Board", "School", "User")
